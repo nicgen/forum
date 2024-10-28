10a85cde3dacc8bf0d44333aa97be15d68693b96
dprouet <damienprouet76@gmail.com>
2024-10-28 13:53:15 +0100

    FixedLogin

--- handlers/login.go
+++ handlers/login.go
@@ -12,67 +12,59 @@ import (
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	db := lib.GetDB()
+	print("test")
 
-	if r.Method == "GET" {
-		http.ServeFile(w, r, "./templates/login.html")
+	email := r.FormValue("EmailForm")
+	password := r.FormValue("PasswordForm")
+
+	// if email == "" || password == "" {
+	// 	http.Error(w, "Email and password fields are mandatory", http.StatusBadRequest)
+	// 	return
+	// }
+
+	// Prepared request to avoid SQL injection
+	stmt, err := db.Prepare("SELECT password FROM User WHERE email = ?")
+	if err != nil {
+		http.Error(w, "Error preparing query", http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Close()
+
+	var hashedPassword string
+	err = stmt.QueryRow(email).Scan(&hashedPassword)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "Invalid email or password", http.StatusUnauthorized)
+		} else {
+			http.Error(w, "Error retrieving user data", http.StatusInternalServerError)
+		}
 		return
 	}
 
-	if r.Method == "POST" {
-		email := r.FormValue("EmailForm")
-		password := r.FormValue("PasswordForm")
-
-		if email == "" || password == "" {
-			http.Error(w, "Email and password fields are mandatory", http.StatusBadRequest)
-			return
-		}
-
-		// Prepared request to avoid SQL injection
-		stmt, err := db.Prepare("SELECT password FROM users WHERE email = ?")
-		if err != nil {
-			http.Error(w, "Error preparing query", http.StatusInternalServerError)
-			return
-		}
-		defer stmt.Close()
-
-		var hashedPassword string
-		err = stmt.QueryRow(email).Scan(&hashedPassword)
-		if err != nil {
-			if err == sql.ErrNoRows {
-				http.Error(w, "Invalid email or password", http.StatusUnauthorized)
-			} else {
-				http.Error(w, "Error retrieving user data", http.StatusInternalServerError)
-			}
-			return
-		}
-
-		// Verify password
-		if !CheckPassword(hashedPassword, password) {
-			http.Error(w, "Invalid email or password", http.StatusUnauthorized)
-			return
-		}
-
-		// Successful login, create a session token
-		sessionUUID, err := uuid.NewV4()
-		if err != nil {
-			http.Error(w, "Error generating session token", http.StatusInternalServerError)
-			return
-		}
-		sessionToken := sessionUUID.String()
-
-		http.SetCookie(w, &http.Cookie{
-			Name:     "session_token",
-			Value:    sessionToken,
-			Expires:  time.Now().Add(24 * time.Hour),
-			HttpOnly: true,
-			Secure:   true,
-			SameSite: http.SameSiteNoneMode,
-		})
-
-		http.Redirect(w, r, "/index", http.StatusSeeOther)
-	} else {
-		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
+	// Verify password
+	if !CheckPassword(hashedPassword, password) {
+		http.Error(w, "Invalid email or password", http.StatusUnauthorized)
+		return
 	}
+
+	// Successful login, create a session token
+	sessionUUID, err := uuid.NewV4()
+	if err != nil {
+		http.Error(w, "Error generating session token", http.StatusInternalServerError)
+		return
+	}
+	sessionToken := sessionUUID.String()
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    sessionToken,
+		Expires:  time.Now().Add(24 * time.Hour),
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteNoneMode,
+	})
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 // Function to check if the password matches the stored hash
