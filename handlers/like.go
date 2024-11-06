0db0f06cdff82452d10ff5b6cd6389c9ce49b04b
dprouet <damienprouet76@gmail.com>
2024-11-06 10:58:42 +0100

    Index fixed

--- handlers/like.go
+++ handlers/like.go
@@ -26,6 +26,8 @@ func LikeHandler(w http.ResponseWriter, r *http.Request) {
 	state_isliked := `SELECT IsLiked FROM Reaction WHERE User_UUID = ? AND Post_ID = ?`
 	err_isliked := db.QueryRow(state_isliked, session_id[0].Value, post_id).Scan(&is_liked)
 
+	println("is liked: ", is_liked)
+
 	if err_isliked == sql.ErrNoRows {
 		state_isliked := `INSERT INTO Reaction (Post_ID, User_UUID, IsLiked) VALUES (?, ?, ?)`
 		_, err_add_react := db.Exec(state_isliked, post_id, session_id[0].Value, is_liked)
@@ -44,14 +46,6 @@ func LikeHandler(w http.ResponseWriter, r *http.Request) {
 		// Redirect User to the home page
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	} else {
-		// // Getting the ID from the database
-		// var user_id string
-		// state_id := `SELECT User_ID FROM User WHERE UUID = ?`
-		// err_user := db.QueryRow(state_id, session_id[0].Value).Scan(&user_id)
-		// if err_user != nil {
-		// 	http.Error(w, "Error accessing User ID", http.StatusUnauthorized)
-		// 	return
-		// }
 
 		// Getting the number of likes of the post in the database
 		var like_number int
