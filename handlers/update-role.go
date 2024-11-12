b5d983e57f71b51a92e29b36aebb6ae76eaa2ba9
ndossant <n.dossantos@outlook.fr>
2024-11-12 08:37:25 +0100

    update role on work

--- handlers/update-role.go
+++ handlers/update-role.go
@@ -56,3 +56,27 @@ func RemoveModerator(w http.ResponseWriter, r *http.Request) {
 	// Redirect or send a success message
 	http.Redirect(w, r, "/profile", http.StatusSeeOther)
 }
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	db := lib.GetDB()
+	// Check if the request method is POST
+	if r.Method != http.MethodPost {
+		ErrorServer(w, "Invalid request method.")
+	}
+
+	// Retrieve the user's UUID from the form
+	userUUID := r.FormValue("userUUID")
+	if userUUID == "" {
+		ErrorServer(w, "User UUID is required.")
+	}
+
+	query := `UPDATE User SET Role = 'DeleteUser' WHERE UUID = ?`
+	_, err := db.Exec(query, userUUID)
+	if err != nil {
+		ErrorServer(w, "Failed to update user role.")
+		fmt.Println("Error updating user role:", err)
+	}
+
+	// Redirect or send a success message
+	http.Redirect(w, r, "/profile", http.StatusSeeOther)
+}
