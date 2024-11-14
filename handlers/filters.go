ae7d080196245f3cb124e48825c3f1de75e5fb78
dprouet <damienprouet76@gmail.com>
2024-11-14 12:42:27 +0100

    Added Username to posts

--- handlers/filters.go
+++ handlers/filters.go
@@ -1,14 +1,125 @@
 package handlers
 
 import (
+	"database/sql"
 	"forum/cmd/lib"
 	"forum/models"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 func FiltersHandler(w http.ResponseWriter, r *http.Request) {
-	// Getting the databa
+	db := lib.GetDB()
+
+	formValues := r.URL.Query()
+
+	// categories := formValues.Get("Category")
+	numberlike := formValues.Get("Like")
+	numberdislike := formValues.Get("Dislike")
+	period := formValues.Get("Period")
+
+	println("Like: ", numberlike)
+	println("Dislike: ", numberdislike)
+	println("Period: ", period)
+
+	// Getting the number of values stored in the Categories tables
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM Categories").Scan(&count)
+	if err != nil {
+		// Handle error
+	}
+
+	// filter category
+	category := ""
+	for i := 0; i < count; i++ {
+		compteur := strconv.Itoa(i)
+		notempty := formValues.Get("Category" + compteur)
+		if len(notempty) != 0 {
+			category = notempty
+		}
+	}
+
+	// Filter like
+	likemin, likemax := 0, 0
+	switch numberlike {
+	case "1-10":
+		likemin = 1
+		likemax = 10
+	case "11-50":
+		likemin = 11
+		likemax = 50
+	case "51-100":
+		likemin = 51
+		likemax = 100
+	case "plus-de-100":
+		likemin = 101
+	}
+
+	// filter dislike
+	dislikemin, dislikemax := 0, 0
+	switch numberdislike {
+	case "1-10":
+		dislikemin = 1
+		dislikemax = 10
+	case "11-50":
+		dislikemin = 11
+		dislikemax = 50
+	case "51-100":
+		dislikemin = 51
+		dislikemax = 100
+	case "plus-de-100":
+		dislikemin = 101
+	}
+
+	//filter period
+	lastperiod := ""
+	switch period {
+	case "week":
+		lastperiod = "-7 days"
+	case "month":
+		lastperiod = "start of month"
+	case "year":
+		lastperiod = "start of year"
+	}
+	var posts []*models.Post
+	var err_post error
+
+	state_filters :=
+		`SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID
+	FROM posts
+	WHERE 
+  (Category_ID = ?)
+	AND
+	(Like BETWEEN ? AND ? OR (? = 0 AND ? = 0)) 
+	AND 
+	(Dislike BETWEEN ? AND ? OR (? = 0 AND ? = 0))
+	AND 
+	(CreatedAt >= DATE('now', ?) AND CreatedAt < DATE('now', 'start of month', '+1 month')) 
+	 `
+	rows, err_post := db.Query(state_filters, category, likemin, likemax, likemin, likemax, dislikemin, dislikemax, dislikemin, dislikemax, lastperiod)
+
+	if err_post != nil {
+
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var post models.Post
+		if err := rows.Scan(&post.ID, &post.Category_ID, &post.Title, &post.Text, &post.Like, &post.Dislike, &post.CreatedAt, &post.User_UUID); err != nil {
+
+		}
+
+		posts = append(posts, &post)
+	}
+
+	if err := rows.Err(); err != nil {
+	}
+	data := lib.DataTest(w, r)
+	data["Posts"] = posts
+
+	// Redirect User to the home page
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func Category(w http.ResponseWriter, r *http.Request) {
@@ -107,11 +218,17 @@ func Like(w http.ResponseWriter, r *http.Request) {
 	}
 	// Users posts Request
 	var posts []*models.Post
+	var rows *sql.Rows
 	var err_post error
 
 	// Making the database request
-	state_like := `SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID FROM Posts`
-	rows, err_post := db.Query(state_like)
+	if !nothing {
+		state_like := `SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID FROM Posts WHERE Like BETWEEN ? AND ?`
+		rows, err_post = db.Query(state_like, minlike, maxlike)
+	} else {
+		state_like := `SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID FROM Posts`
+		rows, err_post = db.Query(state_like)
+	}
 
 	if err_post != nil {
 
@@ -123,9 +240,7 @@ func Like(w http.ResponseWriter, r *http.Request) {
 		if err := rows.Scan(&post.ID, &post.Category_ID, &post.Title, &post.Text, &post.Like, &post.Dislike, &post.CreatedAt, &post.User_UUID); err != nil {
 
 		}
-		if (post.Like > minlike && post.Like < maxlike) || nothing {
-			posts = append(posts, &post)
-		}
+		posts = append(posts, &post)
 	}
 
 	if err := rows.Err(); err != nil {
@@ -162,11 +277,17 @@ func Dislike(w http.ResponseWriter, r *http.Request) {
 	}
 	// Users posts Request
 	var posts []*models.Post
+	var rows *sql.Rows
 	var err_post error
 
 	// Making the database request
-	state_dislike := `SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID FROM Posts`
-	rows, err_post := db.Query(state_dislike)
+	if !nothing {
+		state_dislike := `SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID FROM Posts WHERE Dislike BETWEEN ? AND ?`
+		rows, err_post = db.Query(state_dislike, mindislike, maxdislike)
+	} else {
+		state_dislike := `SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID FROM Posts`
+		rows, err_post = db.Query(state_dislike)
+	}
 
 	if err_post != nil {
 
@@ -190,19 +311,55 @@ func Dislike(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreationDate(w http.ResponseWriter, r *http.Request) {
-	// db := lib.GetDB()
+	db := lib.GetDB()
 	// start := time.Now().Format("2006-01-02 15:04:05")
 
-	// Time := strings.Split(start, " ")
-	// Date := strings.Split(Time[0], "-")
-	// Hours := strings.Split(Time[1],":")
+	// lastweek := now.AddDate(0, 0, -7)
+	// lastmonth := now.AddDate(0, -1, 0)
+	// lastyear := now.AddDate(-1, 0, 0)
 
-	// Year := Date[0]
-	// Month := Date[1]
-	// Day := Date[2]
+	formValues := r.URL.Query()
+	period := formValues.Get("Period")
 
-	// Hour := Hours[0]
-	// Min := Hours[1]
-	// PENSE BETE : ATTENTION AU DEBUT DE MOIS -7 && meme chose pour debut d'année (janvier)
+	var posts []*models.Post
+	var err_post error
 
+	state_period := `SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID FROM Posts`
+	rows, err_post := db.Query(state_period)
+
+	if err_post != nil {
+
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var post models.Post
+		if err := rows.Scan(&post.ID, &post.Category_ID, &post.Title, &post.Text, &post.Like, &post.Dislike, &post.CreatedAt, &post.User_UUID); err != nil {
+
+		}
+		// Time := strings.Split(post.CreatedAt.Format(2024), " ")
+		// Date := strings.Split(Time[0], "-")
+		// Hours := strings.Split(Time[1], ":")
+
+		now := time.Now()
+		lastperiod := now
+		nothing := false
+		switch period {
+		case "Last Week":
+			lastperiod = now.AddDate(0, 0, -7)
+		case "Last Month":
+			lastperiod = now.AddDate(0, -1, 0)
+		case "last Year":
+			lastperiod = now.AddDate(-1, 0, 0)
+		default:
+			nothing = true
+		}
+		display := post.CreatedAt.Before(lastperiod)
+		if !display || nothing {
+			posts = append(posts, &post)
+		}
+	}
+
+	if err := rows.Err(); err != nil {
+	}
 }
