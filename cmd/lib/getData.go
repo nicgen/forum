40e9d527e6c8031702bff039575092c6c669c95a
dprouet <damienprouet76@gmail.com>
2024-11-15 11:31:02 +0100

    Comments added in index aswell

--- cmd/lib/getData.go
+++ cmd/lib/getData.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"forum/models"
 	"net/http"
+	"strings"
 )
 
 func GetData(db *sql.DB, uuid string, status string, page string, r *http.Request) (map[string]interface{}, string) {
@@ -67,6 +68,10 @@ func GetData(db *sql.DB, uuid string, status string, page string, r *http.Reques
 				return nil, "Error scanning user posts"
 			}
 
+			time_post := strings.Split(post.CreatedAt.Format("2006-01-02 15:04:05"), " ")
+			post.Creation_Date = time_post[0]
+			post.Creation_Hour = time_post[1]
+
 			// Getting the Username of the person who made the post
 			state_username := `SELECT Username FROM User WHERE UUID = ?`
 			err_db := db.QueryRow(state_username, post.User_UUID).Scan(&post.Username)
@@ -91,6 +96,10 @@ func GetData(db *sql.DB, uuid string, status string, page string, r *http.Reques
 				if err := rows_comment.Scan(&comment.ID, &comment.Text, &comment.Like, &comment.Dislike, &comment.CreatedAt, &comment.User_UUID, &comment.Post_ID); err != nil {
 					return nil, "Error scanning posts comments"
 				}
+
+				time_comment := strings.Split(comment.CreatedAt.Format("2006-01-02 15:04:05"), " ")
+				comment.Creation_Date = time_comment[0]
+				comment.Creation_Hour = time_comment[1]
 
 				// Getting the Username of the person who made the comment
 				state_username := `SELECT Username FROM User WHERE UUID = ?`
@@ -185,6 +194,49 @@ func GetData(db *sql.DB, uuid string, status string, page string, r *http.Reques
 			if err_db != nil {
 				return nil, "Error getting User's Username"
 			}
+			state_comment := `SELECT ID, Text, Like, Dislike, CreatedAt, User_UUID, Post_ID FROM Comments WHERE Post_ID = ? ORDER BY CreatedAt DESC`
+			fmt.Println("post_id getdata: ", post.ID)
+			// Users posts Request
+			var comments []*models.Comment
+			var rows_comment *sql.Rows
+			rows_comment, err_comment := db.Query(state_comment, post.ID)
+			if err_comment != nil {
+				return nil, "Error accessing user comments"
+			}
+
+			defer rows_comment.Close()
+
+			for rows_comment.Next() {
+				var comment models.Comment
+				if err := rows_comment.Scan(&comment.ID, &comment.Text, &comment.Like, &comment.Dislike, &comment.CreatedAt, &comment.User_UUID, &comment.Post_ID); err != nil {
+					return nil, "Error scanning posts comments"
+				}
+
+				time_comment := strings.Split(comment.CreatedAt.Format("2006-01-02 15:04:05"), " ")
+				comment.Creation_Date = time_comment[0]
+				comment.Creation_Hour = time_comment[1]
+
+				// Getting the Username of the person who made the comment
+				state_username := `SELECT Username FROM User WHERE UUID = ?`
+				err_db := db.QueryRow(state_username, comment.User_UUID).Scan(&comment.Username)
+				if err_db != nil {
+					return nil, "Error getting User's Username for the comment"
+				}
+
+				// comment.Post_ID = post.ID
+
+				// post.Comments = data_post
+				comments = append(comments, &comment)
+				fmt.Println("current struct: ", comment)
+				fmt.Println("comments struct: ", comments)
+			}
+
+			if err := rows.Err(); err != nil {
+				return nil, "Error iterating over user posts"
+			}
+
+			post.Comments = comments
+			post.Data = data_post
 
 			post.Data = data_post
 			posts = append(posts, &post)
