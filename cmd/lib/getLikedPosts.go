efda6a5084104c30e3402cb1d860bec9c5fba2b5
dprouet <damienprouet76@gmail.com>
2024-11-18 11:22:16 +0100

    Liked posts and comments for Users

--- cmd/lib/getLikedPosts.go
+++ cmd/lib/getLikedPosts.go
@@ -1,14 +1,16 @@
 package lib
 
 import (
+	"database/sql"
 	"forum/models"
 	"net/http"
+	"strings"
 )
 
-// ? Function to get liked posts from an User to store it into a data map
+// ? Function to get liked comments from an User to store it into a data map
 func GetLikedPosts(w http.ResponseWriter, uuid string, data map[string]interface{}) map[string]interface{} {
 	// Making query for the posts liked by the User
-	state_liked := `SELECT Post_ID FROM Reaction WHERE User_UUID = ?`
+	state_liked := `SELECT Post_ID FROM Reaction WHERE User_UUID = ? AND Post_ID IS NOT NULL`
 	query, err_liked := db.Query(state_liked, uuid)
 	if err_liked != nil {
 		ErrorServer(w, "Error accessing user's Reactions")
@@ -20,7 +22,7 @@ func GetLikedPosts(w http.ResponseWriter, uuid string, data map[string]interface
 	reaction := ""
 
 	for query.Next() {
-		if err := query.Scan(&reaction); err != nil {
+		if err := query.Scan(&reaction); err != nil && err != sql.ErrNoRows {
 			ErrorServer(w, "Error scanning user's Reactions")
 		}
 
@@ -42,6 +44,10 @@ func GetLikedPosts(w http.ResponseWriter, uuid string, data map[string]interface
 			if err := row_post.Scan(&post_liked.ID, &post_liked.Category_ID, &post_liked.Title, &post_liked.Text, &post_liked.Like, &post_liked.Dislike, &post_liked.CreatedAt, &post_liked.User_UUID); err != nil {
 				ErrorServer(w, "Error scanning posts data")
 			}
+
+			time_comment := strings.Split(post_liked.CreatedAt.Format("2006-01-02 15:04:05"), " ")
+			post_liked.Creation_Date = time_comment[0]
+			post_liked.Creation_Hour = time_comment[1]
 
 			posts_liked = append(posts_liked, &post_liked)
 		}
