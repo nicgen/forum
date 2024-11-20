743dd5a65f7b40fb14290e9bcf22d78732c5be3b
ndossant <n.dossantos@outlook.fr>
2024-11-20 11:28:38 +0100

    Notification for comment

--- cmd/lib/getNotification.go
+++ cmd/lib/getNotification.go
@@ -3,16 +3,15 @@ package lib
 import (
 	"forum/models"
 	"net/http"
-	"time"
 )
 
 // GetNotifications - Function to retrieve notifications for a user and store them in the data map
 func GetNotifications(w http.ResponseWriter, userUUID string, data map[string]interface{}) map[string]interface{} {
 	// Retrieve all notifications for the given user
-	var notifications []models.Notification
+	var notifications []*models.Notification
 
 	notificationsQuery := `
-		SELECT ID, Reaction_ID, Post_ID, Comment_ID, CreatedAt, IsRead	FROM Notification WHERE User_UUID = ?ORDER BY CreatedAt DESC`
+		SELECT ID, Post_ID, Comment_ID, CreatedAt, IsRead	FROM Notification WHERE User_UUID = ? ORDER BY CreatedAt DESC`
 
 	rows, err := db.Query(notificationsQuery, userUUID)
 	if err != nil {
@@ -23,22 +22,22 @@ func GetNotifications(w http.ResponseWriter, userUUID string, data map[string]in
 
 	for rows.Next() {
 		var notification models.Notification
-		var createdAt string
+		// var createdAt string
 
 		// Scan the results into the Notification struct
-		if err := rows.Scan(&notification.ID, &notification.ReactionID, &notification.PostID, &notification.CommentID, &createdAt, &notification.IsRead); err != nil {
+		if err := rows.Scan(&notification.ID, &notification.PostID, &notification.CommentID, &notification.CreatedAt, &notification.IsRead); err != nil {
 			ErrorServer(w, "Error scanning notifications")
 			return data
 		}
 
-		// Convert the createdAt string to a time.Time
-		notification.CreatedAt, err = time.Parse("2006-01-02 15:04:05", createdAt)
-		if err != nil {
-			ErrorServer(w, "Error parsing notification timestamp")
-			return data
-		}
+		// // Convert the createdAt string to a time.Time
+		// notification.CreatedAt, err = time.Parse("2006-01-02 15:04:05", createdAt)
+		// if err != nil {
+		// 	ErrorServer(w, "Error parsing notification timestamp")
+		// 	return data
+		// }
 
-		notifications = append(notifications, notification)
+		notifications = append(notifications, &notification)
 	}
 
 	if err := rows.Err(); err != nil {
