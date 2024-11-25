27e3797b4653aa341a675eb16ca1571597f3fd68
ahoussay <aurelie.houssaye.com>
2024-11-25 17:33:27 +0100

    Profile when clicking on Post's User fixed

--- handlers/postCreation.go
+++ handlers/postCreation.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"forum/cmd/lib"
 	"net/http"
 	"time"
@@ -11,18 +12,31 @@ func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
 	// Storing database into a variable
 	db := lib.GetDB()
 
+    // Parse the form data (including query parameters and form body)
+    err := r.ParseForm()
+    if err != nil {
+        fmt.Println("Error parsing form:", err)
+        http.Error(w, "Unable to parse form data", http.StatusInternalServerError)
+        return
+    }
+	
 	// Getting the cookie (containing the UUID)
 	cookie, _ := r.Cookie("session_id")
-
+	
 	// Storing the form values into variables
 	title := r.FormValue("post_title")
 	text := r.FormValue("post_text")
-	category := r.FormValue("post_category")
+    selectedCategories := r.Form["categories"] // This gives you a slice of strings
+
+	var categories string
+	for i := 0; i < len(selectedCategories); i++ {
+		categories += selectedCategories[i] + ","
+	}
 
 	var like_count, dislike_count int = 0, 0
 	// Storing those values into the database with a database request
 	state_post := `INSERT INTO Posts (User_UUID, Title, Category_ID, Text, Like, Dislike, CreatedAt) VALUES (?, ?, ?, ?, ?, ?, ?)`
-	_, err_db := db.Exec(state_post, cookie.Value, title, category, text, like_count, dislike_count, time.Now())
+	_, err_db := db.Exec(state_post, cookie.Value, title, categories[:len(categories)-1], text, like_count, dislike_count, time.Now())
 	if err_db != nil {
 		lib.ErrorServer(w, "Error inserting new Post")
 	}
