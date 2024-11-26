6b0eb6f74998c49510075bb0ce7a6d70350a27a7
ahoussay <aurelie.houssaye.com>
2024-11-26 10:10:18 +0100

    Corrected merge

--- handlers/profile.go
+++ handlers/profile.go
@@ -31,7 +31,7 @@ func ProfileUserHandler(w http.ResponseWriter, r *http.Request) {
 	// Getting the User Data
 	data := lib.GetData(db, user_uuid, "logged", "profile_user", w, r)
 	data = lib.GetComments(db, user_uuid, data, w, r)
-	data = lib.GetNotifications(w, user_uuid, data)
+	// data = lib.GetNotifications(w, user_uuid, data)
 
 	// Redirect User to the profile html page and sending the data to it
 	lib.RenderTemplate(w, "layout/index", "page/profile", data)
