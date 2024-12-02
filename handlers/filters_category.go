a15f9a66e563209b5bd26980319f67e2cd2223cf
Golden <damienprouet76@gmail.com>
2024-12-02 12:16:28 +0100

    Filters almost fixed

--- handlers/filters_category.go
+++ handlers/filters_category.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"database/sql"
 	"forum/cmd/lib"
 	"forum/models"
 	"net/http"
@@ -12,11 +13,29 @@ func Filters_Category(w http.ResponseWriter, r *http.Request) {
 	formValues := r.URL.Query()
 	categories := formValues.Get("Category")
 
+	var role, uuid string
+	cookie_role, err_role := r.Cookie("role")
+	cookie_uuid, err_role := r.Cookie("session_id")
+	if err_role == http.ErrNoCookie {
+		role = "Guest"
+	} else {
+		role = cookie_role.Value
+	}
+	if err_role == http.ErrNoCookie {
+		uuid = "nill"
+	} else {
+		uuid = cookie_uuid.Value
+	}
+
+	data_user := map[string]interface{}{
+		"Role":	role,
+	}
+
 	var posts []*models.Post
 	var err_post error
 
 	state_filters :=
-		`SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID, ImagePath FROM Posts`
+		`SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID, ImagePath FROM Posts ORDER BY CreatedAt DESC`
 
 	rows, err_post := db.Query(state_filters)
 
@@ -42,6 +61,74 @@ func Filters_Category(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 		if is_contained {
+			// Getting the Username of the person who made the post
+			post.Username = lib.CheckUsername(w, post.User_UUID)
+	
+			if data_user["Role"] != "Guest" {
+				// Check the post status with user's uuid and post id
+				post.Status = lib.CheckStatus(w, uuid, post.ID, "post")
+	
+				// Check if the post is from the User making the request
+				if post.User_UUID == uuid {
+					post.IsAuthor = "yes"
+				} else {
+					post.IsAuthor = "no"
+				}
+			}
+	
+			state_comment := `SELECT ID, Text, Like, Dislike, CreatedAt, User_UUID, Post_ID FROM Comments WHERE Post_ID = ? ORDER BY CreatedAt DESC`
+			// Users posts Request
+			var comments []*models.Comment
+			var rows_comment *sql.Rows
+			rows_comment, err_comment := db.Query(state_comment, post.ID)
+			if err_comment != nil {
+				lib.ErrorServer(w, "Error accessing user comments")
+			}
+	
+			defer rows_comment.Close()
+	
+			for rows_comment.Next() {
+				var comment models.Comment
+				if err := rows_comment.Scan(&comment.ID, &comment.Text, &comment.Like, &comment.Dislike, &comment.CreatedAt, &comment.User_UUID, &comment.Post_ID); err != nil {
+					lib.ErrorServer(w, "Error scanning posts comments")
+				}
+	
+				time_comment := strings.Split(comment.CreatedAt.Format("2006-01-02 15:04:05"), " ")
+				comment.Creation_Date = time_comment[0]
+				comment.Creation_Hour = time_comment[1]
+	
+				// Getting the Username of the person who made the comment
+				comment.Username = lib.CheckUsername(w, comment.User_UUID)
+	
+				if data_user["Role"] != "Guest" {
+					// Check the post status with user's uuid and post id
+					post.Status = lib.CheckStatus(w, uuid, post.ID, "comment")
+	
+					// Check if the post is from the User making the request
+					if post.User_UUID == uuid {
+						comment.IsAuthor = "yes"
+					} else {
+						comment.IsAuthor = "no"
+					}
+				}
+				data_comment := map[string]interface{}{
+					"Role":	role,
+				}
+				comment.Data = data_comment
+	
+				comments = append(comments, &comment)
+			}
+	
+			if err := rows.Err(); err != nil {
+				lib.ErrorServer(w, "Error iterating over user comments")
+			}
+	
+			post.Comments = comments
+	
+			data_post := map[string]interface{}{
+				"Role": role,
+			}
+			post.Data = data_post
 			posts = append(posts, &post)
 		}
 	}
