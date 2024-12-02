8071e7e3f023b331eb06ee78d61a79bd757be257
Golden <damienprouet76@gmail.com>
2024-12-02 16:09:12 +0100

    Fixed range comments and modified Profile html

--- cmd/lib/getComments.go
+++ cmd/lib/getComments.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"database/sql"
+	"fmt"
 	"forum/models"
 	"net/http"
 	"strings"
@@ -27,9 +28,10 @@ func GetComments(db *sql.DB, uuid string, data map[string]interface{}, w http.Re
 		}
 
 		var post models.Post
-		state_post := `SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID FROM Posts WHERE User_UUID = ? ORDER BY CreatedAt DESC`
-		err_comment := db.QueryRow(state_post, uuid).Scan(&post.ID, &post.Category_ID, &post.Title, &post.Text, &post.Like, &post.Dislike, &post.CreatedAt, &post.User_UUID)
+		state_post := `SELECT ID, Category_ID, Title, Text, Like, Dislike, CreatedAt, User_UUID FROM Posts WHERE ID = ? ORDER BY CreatedAt DESC`
+		err_comment := db.QueryRow(state_post, comment.Post_ID).Scan(&post.ID, &post.Category_ID, &post.Title, &post.Text, &post.Like, &post.Dislike, &post.CreatedAt, &post.User_UUID)
 		if err_comment != nil {
+			fmt.Println("error: ", err_comment)
 			ErrorServer(w, "Error getting post infos")
 		}
 
