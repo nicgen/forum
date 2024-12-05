fc47c3f65c90eeed7251f8cb12bf150ade57c285
djustine <justinedenieulle@gmail.com>
2024-12-05 14:27:38 +0100

    page about finished

--- handlers/about.go
+++ handlers/about.go
@@ -2,22 +2,12 @@ package handlers
 
 import (
 	"forum/cmd/lib"
-	"forum/models"
-	"html/template"
 	"net/http"
 )
 
 // IndexHandler handles requests to the /about URL
 func AboutHandler(w http.ResponseWriter, r *http.Request) {
-	data := models.PageData{
-		Title:  "About",
-		Header: "About Us",
-		Content: map[string]template.HTML{
-			"Msg_raw":    "<h1>Title 01.</h1><p>paragraph</>",
-			"Msg_styled": "<h1 style=\"text=color: blue;\">Title 01.</h1><p>paragraph with</br>style</>",
-		},
-		IsError: false,
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	data := lib.DataTest(w, r)
+	data = lib.ErrorMessage(w, data, "none")
 	lib.RenderTemplate(w, "layout/index", "page/about", data)
 }
